5094e8ca2b44f995f0c70b538f599a52c9d89623
James Brennen <jbjamesbrennen@gmail.com>
2024-04-11 22:35:20 +0100

    Added playRound function - determines the result of the round as 0: loss, 1: draw, and 2: win, and returns the value.

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function getComputerChoice() {
     let computerChoice = choices[randomChoiceNum];
     return computerChoice;
 }
+
+function playRound(computerSelection, playerSelection) {
+    let result = 0;
+    if (computerSelection === playerSelection) {
+        result = 1;
+    } else if (computerSelection === "rock" && playerSelection === "paper") {
+        result = 2;
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        result = 0;
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        result = 0;
+    } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        result = 2;
+    } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        result = 2;
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        result = 0;
+    }
+
+    return result;
+}
