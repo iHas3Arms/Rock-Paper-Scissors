349651b34c24b99824d30aa9596998234a980038
James Brennen <jbjamesbrennen@gmail.com>
2024-04-15 22:10:32 +0100

    Added style.css file to style UI, changed some JavaScript by removing the playGame() function to play a round each time a button is pressed. Added event listeners for each button so a round is played onclick.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+let buttonRock = document.getElementById("rock");
+let buttonPaper = document.getElementById("paper");
+let buttonScissors = document.getElementById("scissors");
+let allButtons = [buttonRock, buttonPaper, buttonScissors];
+
 let choices = ["rock", "paper", "scissors"];
 let amountOfRounds = 5;
+
+buttonRock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
+
+buttonPaper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+});
+
+buttonScissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+});
 
 function getComputerChoice() {
     let randomChoiceNum = Math.floor(Math.random() * 3);
@@ -7,14 +24,14 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getUserChoice() {
-    let userChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
+// function getUserChoice() {
+//     let userChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
 
-    const checkInList = (choice) => choice === userChoice;
+//     const checkInList = (choice) => choice === userChoice;
 
-    if (!choices.some(checkInList)) return getComputerChoice();
-    return userChoice;
-}
+//     if (!choices.some(checkInList)) return getComputerChoice();
+//     return userChoice;
+// }
 
 function getRoundResult(playerSelection, computerSelection) {
     let result = 0;
@@ -56,28 +73,29 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-function playGame() {
-    console.log("============================================\n============================================")
-    let score = 0;
-    for (i=0;i<amountOfRounds;i++) {
-        score += playRound(getUserChoice(), getComputerChoice());
-        console.log(score);
-    }
-    console.log(`You got a score of ${score} out of ${amountOfRounds}`);
-    askToPlayAgain();
-}
+// function playGame() {
+//     console.log("============================================\n============================================")
+//     let score = 0;
+//     for (i=0;i<amountOfRounds;i++) {
+//         score += playRound(getUserChoice(), getComputerChoice());
+//         console.log(score);
+//     }
+//     console.log(`You got a score of ${score} out of ${amountOfRounds}`);
+//     askToPlayAgain();
+// }
 
-function askToPlayAgain() {
-    console.log("Would you like to play again?");
-    let userInput = prompt("Would you like to play again? Choose yes or no: ").toLowerCase();
-    if (userInput === "yes") {
-        playGame();
-    } else if (userInput === "no") {
-        console.log("See you next time.");
-    } else {
-        console.log("Please type either 'yes' or 'no' to the following question.");
-        askToPlayAgain();
-    }
-}
+// function askToPlayAgain() {
+//     console.log("Would you like to play again?");
+//     let userInput = prompt("Would you like to play again? Choose yes or no: ").toLowerCase();
+//     if (userInput === "yes") {
+//         playGame();
+//     } else if (userInput === "no") {
+//         console.log("See you next time.");
+//         console.log("============================================\n============================================");
+//     } else {
+//         console.log("Please type either 'yes' or 'no' to the following question.");
+//         askToPlayAgain();
+//     }
+// }
 
-playGame();
+// playGame();
